54fecb0ef3d832f525a7a06f3d37c33d6ebcf3ab
AntoineCdm <antoine.cdm@orange.fr>
2021-02-10 20:12:14 +0100

    init project

---
+++ src/main/java/com/pluralsight/pluralsightfisrtspringbootapp/PluralsightFisrtSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.pluralsightfisrtspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightFisrtSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightFisrtSpringBootAppApplication.class, args);
+	}
+
+}
